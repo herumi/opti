44c9b78c2c8621dad50290e18a5b19143f442cd1
MITSUNARI Shigeo <herumi@nifty.com>
2011-06-24 16:23:55 +0900

    add sequential access

--- icache.cpp
+++ icache.cpp
@@ -5,7 +5,7 @@
 const int N = 0x1000000;
 
 struct Code : Xbyak::CodeGenerator {
-	explicit Code(int count)
+	explicit Code(int count, int mode)
 		: CodeGenerator(1024 * 1024 * 64)
 	{
 		using namespace Xbyak;
@@ -13,18 +13,22 @@ struct Code : Xbyak::CodeGenerator {
 		xor(eax, eax);
 	L(".lp");
 		for (int i = 0; i < count; i++) {
-			L(Label::toStr(i).c_str());
-			add(eax, 1);
-			int to = 0;
-			if (i < count / 2) {
-				to = count - 1 - i;
+			if (mode == 0) {
+				add(eax, 1);
 			} else {
-				to = count  - i;
-			}
-			if (i == count / 2) {
-				jmp(".exit", T_NEAR);
-			} else {
-				jmp(Label::toStr(to).c_str(), T_NEAR);
+				L(Label::toStr(i).c_str());
+				add(eax, 1);
+				int to = 0;
+				if (i < count / 2) {
+					to = count - 1 - i;
+				} else {
+					to = count  - i;
+				}
+				if (i == count / 2) {
+					jmp(".exit", T_NEAR);
+				} else {
+					jmp(Label::toStr(to).c_str(), T_NEAR);
+				}
 			}
 		}
 	L(".exit");
@@ -34,10 +38,10 @@ struct Code : Xbyak::CodeGenerator {
 	}
 };
 
-void test(int count)
+void test(int count, int mode)
 {
-	printf("%8d :", count);
-	Code code(count);
+	printf("%6d :", count);
+	Code code(count, mode);
 	Xbyak::util::Clock clk;
 	const int LP = 4;
 	int sum = 0;
@@ -47,15 +51,20 @@ void test(int count)
 		clk.end();
 	}
 	if (sum != LP * N) printf("err sum=%d, %d\n", sum, LP * N);
-	printf("%6.3fclk code size=%8.3fKbyte\n", clk.getClock() / (double)LP / N, code.getSize() / 1e3);
+	printf("%6.3f, %8.3f\n", clk.getClock() / (double)LP / N, code.getSize() / 1e3);
 }
 
 
 int main()
 {
-	int count = 1;
-	for (int i = 0; i < 18; i++) {
-		test(count);
-		count *= 2;
+	for (int mode = 0; mode < 2; mode++) {
+		printf("[%s]\n", mode == 0 ? "sequential" : "random");
+		printf(" count  clk   code size(Kbyte)\n");
+		int count = 1;
+		for (int i = 0; i < 18; i++) {
+			test(count, mode);
+			count *= 2;
+		}
 	}
 }
+
