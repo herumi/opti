ed37f963cf00b168ab8fc1c26dadc357454962d3
ubuntu <shigeo@avx-linux.(none)>
2011-06-27 21:41:55 +0900

    change output format

--- strlen_sse42.cpp
+++ strlen_sse42.cpp
@@ -3,16 +3,16 @@
 	g++ -O3 -fomit-frame-pointer -march=core2 -msse4 -fno-operator-names strlen_sse42.cpp && ./a.out
 
 	Xeon X5650 2.67GHz + Linux 2.6.32 + gcc 4.6.0
-	ave             2      5      7     10     12     16     20     32     64    128    256    512   1024
-	strlenANSI   12.22   4.96   4.03   3.18   2.87   2.38   2.08   1.55   1.00   0.70   0.51   0.38   0.34
-	strlenC      13.69   8.34   6.86   5.57   5.04   4.34   3.89   3.27   2.74   2.42   2.28   2.19   2.21
-	strlenSSE42   9.29   3.98   3.22   2.66   2.46   2.19   1.99   1.55   0.97   0.68   0.49   0.35   0.30
+	ave           1.98   4.96   6.76   9.75  11.60  15.23  18.86  28.65  51.63  84.67 127.23 175.75 197.63
+	strlenLIBC   12.27   5.01   4.07   3.22   2.91   2.43   2.13   1.57   1.02   0.71   0.52   0.38   0.34
+	strlenC      14.07   8.44   6.91   5.58   5.05   4.34   3.90   3.24   2.74   2.43   2.32   2.23   2.20
+	strlenSSE42   9.32   3.99   3.23   2.71   2.50   2.25   2.03   1.59   1.01   0.70   0.51   0.35   0.31
 
 	Core i7-2600 CPU 3.40GHz + Linux 2.6.35 + gcc 4.4.5
-	ave             2      5      7     10     12     16     20     32     64    128    256    512   1024
-	strlenANSI   11.57   5.69   4.64   3.54   3.10   2.52   2.11   1.39   0.70   0.46   0.35   0.30   0.29
-	strlenC      14.29   6.96   5.55   4.37   3.97   3.42   3.07   2.53   1.97   1.68   1.52   1.44   1.41
-	strlenSSE42  12.51   5.22   4.08   3.26   2.95   2.57   2.28   1.53   0.82   0.51   0.39   0.32   0.30
+	ave           1.98   4.96   6.76   9.75  11.60  15.23  18.86  28.65  51.63  84.67 127.23 175.75 197.63
+	strlenLIBC   11.62   5.66   4.62   3.55   3.10   2.49   2.10   1.41   0.70   0.47   0.37   0.30   0.29
+	strlenC      13.96   6.86   5.48   4.30   3.90   3.34   3.03   2.52   2.03   1.75   1.59   1.49   1.47
+	strlenSSE42  12.62   5.27   4.13   3.28   2.96   2.59   2.31   1.53   0.81   0.52   0.39   0.32   0.29
 */
 #include <stdio.h>
 #include <stdlib.h>
@@ -58,12 +58,13 @@ struct StrlenSSE42 : Xbyak::CodeGenerator {
 
 struct Result {
 	int hit;
+	double len;
 	double time;
 	Result() {}
-	Result(int hit, double time) : hit(hit), time(time) {}
+	Result(int hit, double len, double time) : hit(hit), len(len), time(time) {}
 	void put() const
 	{
-		printf("hit=%d time= %.2f clk\n", hit, time);
+		printf("hit=%d len=%.2f time= %.2f clk\n", hit, len, time);
 	}
 };
 
@@ -81,9 +82,9 @@ void createTable(char *p, size_t num, int ave)
 Result test(const char *top, size_t n, size_t count, size_t func(const char*))
 {
 	Xbyak::util::Clock clk;
-	clk.begin();
 	int hit = 0;
 	for (size_t i = 0; i < count; i++) {
+		clk.begin();
 		const char *p = top;
 		int remain = n;
 		while (remain > 0) {
@@ -92,9 +93,9 @@ Result test(const char *top, size_t n, size_t count, size_t func(const char*))
 			remain -= len + 1;
 			p += len + 1;
 		}
+		clk.end();
 	}
-	clk.end();
-	return Result(hit, clk.getClock() / (double)count / n);
+	return Result(hit, n / (double)hit * count, clk.getClock() / (double)count / n);
 }
 
 size_t strlenC(const char *p)
@@ -124,7 +125,7 @@ int main(int argc, char *argv[])
 		const char *name;
 		size_t (*func)(const char*);
 	} funcTbl[] = {
-		{ "strlenANSI ", strlen },
+		{ "strlenLIBC ", strlen },
 		{ "strlenC    ", strlenC },
 		{ "strlenSSE42", strlenSSE42 },
 	};
@@ -147,9 +148,10 @@ int main(int argc, char *argv[])
 
 	puts("end");
 
-	printf("ave        ");
+	printf("ave         ");
 	for (size_t i = 0; i < NUM_OF_ARRAY(aveTbl); i++) {
-		printf("%6d ", aveTbl[i]);
+//		printf("%6d ", aveTbl[i]);
+		printf("%6.2f ", rv[0][i].len);
 	}
 	printf("\n");
 	for (int i = 0; i < NUM_OF_ARRAY(funcTbl); i++) {
