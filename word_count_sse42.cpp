c27d3675f873dbd37736e69cf3f324bca2e73fd5
MITSUNARI Shigeo <herumi@nifty.com>
2011-08-01 20:25:25 +0900

    add Xbyak version

--- word_count_sse42.cpp
+++ word_count_sse42.cpp
@@ -35,7 +35,7 @@ size_t countWord_C(const char *p)
 {
 	static const char alp_map8[32] = { 0, 0, 0, 0, 0x80, 0, 0xff, 0x3, 0xfe, 0xff, 0xff, 0x7, 0xfe, 0xff, 0xff, 0x7 };
 	size_t i = 1,  cnt = 0;
-	char cc, cc2;
+	unsigned char cc, cc2;
 	bool flag[3];
 	cc2 = cc = p[0];
 	flag[1] = alp_map8[cc >> 3] & (1 << (cc & 7));
@@ -51,6 +51,23 @@ size_t countWord_C(const char *p)
 	return cnt;
 }
 
+static char alnumTbl2[256];
+size_t countWord_C2(const char *p)
+{
+	size_t count = 0;
+	unsigned char c = *p++;
+	char prev = alnumTbl2[c];
+	while (c) {
+		c = *p++;
+		char cur = alnumTbl2[c];
+		if (!cur && prev) {
+			count++;
+		}
+		prev = cur;
+	}
+	return count;
+}
+
 /*
 	see http://msirocoder.blog35.fc2.com/blog-entry-65.html
 */
@@ -61,10 +78,10 @@ size_t countWord_SSE42(const char *p)
 	__m128i ret, x, prev;
 	size_t count = 0;
 	prev = _mm_setzero_si128();
-	goto IN;
+	goto SKIP;
 	do {
 		p += 16;
-	IN:
+	SKIP:
 		ret = _mm_cmpistrm(im, *(const __m128i*)p, 0x4);
 		x = _mm_slli_epi16(ret, 1);
 		x = _mm_or_si128(prev, x);
@@ -81,33 +98,49 @@ struct CountWordSSE42 : Xbyak::CodeGenerator {
 		inLocalLabel();
 		using namespace Xbyak;
 #if defined(XBYAK64_WIN)
-		const Reg64& p1 = rdx;
-		const Reg64& c = rcx;
+		const Reg64& p = rdx;
 		const Reg64& a = rax;
-		mov(rdx, rcx);
+		const Reg64& c = rcx;
 #elif defined(XBYAK64_GCC)
-		const Reg64& p1 = rdi;
-		const Reg64& c = rcx;
+		const Reg64& p = rdi;
 		const Reg64& a = rax;
+		const Reg64& c = rcx;
 #else
-		const Reg32& p1 = edx;
-		const Reg32& c = ecx;
+		const Reg32& p = edx;
 		const Reg32& a = eax;
+		const Reg32& c = ecx;
 		mov(edx, ptr [esp + 4]);
 #endif
-		mov(eax, 0xff01);
-		movd(xm0, eax);
-
-		mov(a, p1);
-		xor(c, c);
+		mov(a, (size_t)alnumTbl);
+		pxor(xm3, xm3);
+		movdqa(xm2, ptr [a]);
+		xor(a, a);
 		jmp(".in");
 	L("@@");
-		add(a, 16);
-	L(".in");
-		pcmpistri(xm0, ptr [a], 0x14);
-		jnz("@b");
+		movdqa(xm4, xm0);
+		psllw(xm4, 1);
+		por(xm4, xm3);
+		movdqa(xm3, xm0);
+		pxor(xm4, xm0);
+		psrld(xm3, 15);
+		movd(Reg32(c.getIdx()), xm4);
+		popcnt(c, c);
+		add(p, 16);
 		add(a, c);
-		sub(a, p1);
+	L(".in");
+		movdqa(xm1, ptr [p]);
+		pcmpistrm(xm2, xm1, 4);
+		jnz("@b");
+		movdqa(xm4, xm0);
+		psllw(xm4, 1);
+		por(xm4, xm3);
+		movdqa(xm3, xm0);
+		pxor(xm4, xm0);
+		psrld(xm3, 15);
+		movd(Reg32(c.getIdx()), xm4);
+		popcnt(c, c);
+		add(a, c);
+		shr(a, 1);
 		ret();
 		outLocalLabel();
 	}
@@ -129,9 +162,33 @@ void test(const char *text, size_t (*countFunc)(const char *))
 
 int main()
 {
+	for (int i = 0; i < 256; i++) {
+		alnumTbl2[i] = (i == '\'') || ('0' <= i && i <= '9') || ('a' <= i && i <= 'z') || ('A' <= i && i <= 'Z');
+	}
 	std::string textBuf;
 	const char *text = LoadFile(textBuf, "test.txt");
 	if (text == 0) return 1;
 	test(text, countWord_C);
+	test(text, countWord_C2);
 	test(text, countWord_SSE42);
+	test(text, countWord_SSE42asm);
+#if 0
+	MIE_ALIGN(16) const char src[] = "ute address DS";
+	int a = countWord_C(src);
+	int b = countWord_SSE42(src);
+	printf("a=%d, b=%d\n", a, b);
+#else
+	size_t len = strlen(text);
+	for (size_t i = 0; i < len; i += 16) {
+		MIE_ALIGN(16) char src[17];
+		memcpy(src, text + i, 16);
+		src[16] = 0;
+		int a = countWord_C(src);
+		int b = countWord_SSE42(src);
+		if (a != b) {
+			printf("pos=%d, a=%d, b=%d\n", i, a, b);
+			printf("[%s]\n", src);
+		}
+	}
+#endif
 }
