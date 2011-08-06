e82c07cab6f0326c91627660452e4316722bbf6c
MITSUNARI Shigeo <herumi@nifty.com>
2011-08-06 13:15:54 +0900

    add findRange

--- strchr_sse42.cpp
+++ strchr_sse42.cpp
@@ -103,6 +103,27 @@ const char *strchrSSE42_C(const char* p, int c)
 	return 0;
 }
 
+const char *findRange_SSE42(const char* p, char c1, char c2)
+{
+	const __m128i im = _mm_set1_epi32(((unsigned char)c1) | (((unsigned char)c2) << 8));
+	while (_mm_cmpistra(im, *(const __m128i*)p, 4)) {
+		p += 16;
+	}
+	if (_mm_cmpistrc(im, *(const __m128i*)p, 4)) {
+		return p + _mm_cmpistri(im, *(const __m128i*)p, 4);
+	}
+	return 0;
+}
+
+const char *findRange_C(const char* p, char c1, char c2)
+{
+	while (*p) {
+		if (c1 <= *p && *p <= c2) return p;
+		p++;
+	}
+	return 0;
+}
+
 void test(const char *str, const char *f(const char*, int))
 {
 	Xbyak::util::Clock clk;
@@ -112,7 +133,34 @@ void test(const char *str, const char *f(const char*, int))
 		clk.begin();
 		for (int c = 1; c <= MaxChar; c++) {
 			const char *p = f(str, c);
-#if 1
+#if 0
+			const char *q = strchr(str, c);
+			if (p != q) {
+				printf("err, c=%d, p=%p(%d), q=%p(%d)\n", c, p, int(p - str), q, int(q - str));
+				exit(1);
+			}
+#endif
+			if (p) {
+				ret += p - str;
+			} else {
+				ret += MaxChar;
+			}
+		}
+		clk.end();
+	}
+	printf("ret=%d, %.3f\n", ret / count, clk.getClock() / (double)ret);
+}
+
+void test2(const char *str, const char *f(const char*, char,char))
+{
+	Xbyak::util::Clock clk;
+	int ret = 0;
+	const int count = 30000;
+	for (int i = 0; i < count; i++) {
+		clk.begin();
+		for (int c = 1; c <= MaxChar; c++) {
+			const char *p = f(str, c, MaxChar);
+#if 0
 			const char *q = strchr(str, c);
 			if (p != q) {
 				printf("err, c=%d, p=%p(%d), q=%p(%d)\n", c, p, int(p - str), q, int(q - str));
@@ -153,6 +201,18 @@ int main(int argc, char *argv[])
 	for (size_t j = 0; j < NUM_OF_ARRAY(funcTbl); j++) {
 		puts(funcTbl[j].name);
 		test(str, funcTbl[j].f);
+	}
+	static const struct {
+		const char *name;
+		const char *(*f)(const char*, char,char);
+	} funcTbl2[] = {
+		{ "findRange_C  ", findRange_C },
+		{ "findRange_SSE42  ", findRange_SSE42 },
+	};
+
+	for (size_t j = 0; j < NUM_OF_ARRAY(funcTbl2); j++) {
+		puts(funcTbl2[j].name);
+		test2(str, funcTbl2[j].f);
 	}
 }
 
