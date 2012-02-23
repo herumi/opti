d0ce45fe3634633fc06a303fb261de38f02d8c3a
MITSUNARI Shigeo <herumi@nifty.com>
2012-02-23 10:50:12 +0900

    print average clock per search length

--- quick_search_smpl.cpp
+++ quick_search_smpl.cpp
@@ -2,6 +2,8 @@
 	compare strstr and quick search
 	require https://github.com/herumi/cybozulib
 	        https://github.com/herumi/xbyak
+	g++ quick_search_smpl.cpp -I../cybozulib/include/ -fno-operator-names -O3 -fomit-frame-pointer -march=native
+	@author herumi
 */
 #include <string>
 #include <vector>
@@ -12,10 +14,12 @@
 
 void test(const std::string& text, const std::string& key)
 {
-	int ret1 = 0;
-	int ret2 = 0;
+	int num1 = 0;
+	int num2 = 0;
 	double time1 = 0;
 	double time2 = 0;
+	double len1 = 0;
+	double len2 = 0;
 	const int N = 10;
 	for (int i = 0; i < N; i++) {
 		{
@@ -27,7 +31,8 @@ void test(const std::string& text, const std::string& key)
 				const char *q = strstr(p, key.c_str());
 				clk.end();
 				if (q == 0) break;
-				ret1 += (int)(q - p);
+				num1++;
+				len1 += (int)(q - p);
 				p = q + 1;
 			}
 			time1 += clk.getClock() / double(clk.getCount()) * 1e-3;
@@ -42,18 +47,26 @@ void test(const std::string& text, const std::string& key)
 				const char *q = qs.find(p, end);
 				clk.end();
 				if (q == 0) break;
-				ret2 += (int)(q - p);
+				num2++;
+				len2 += (int)(q - p);
 				p = q + 1;
 			}
 			time2 += clk.getClock() / double(clk.getCount()) * 1e-3;
 		}
 	}
+	num1 /= N;
+	num2 /= N;
+	len1 /= N;
+	len2 /= N;
 	time1 /= N;
 	time2 /= N;
-	if (ret1 != ret2) {
-		fprintf(stderr, "err key=%s, ret1=%d, ret2=%d\n", key.c_str(), ret1, ret2);
+	if (num1 != num2 || len1 != len2) {
+		fprintf(stderr, "err key=%s, (%d, %d), (%d, %d)\n", key.c_str(), num1, (int)len1, num2, (int)len2);
 	}
-	printf("%30s ret=%8d, strstr:%8.3fK quick:%8.3fK (%5.2f)\n", key.c_str(), ret1 / N, time1, time2, time1 / time2);
+	if (num1) len1 /= num1;
+	double ave1 = len1 ? time1 / len1 * 1e3 : 0;
+	double ave2 = len1 ? time2 / len1 * 1e3 : 0;
+	printf("%27s %6d %10.1f %8.2f %5.2f %8.2f %5.2f %4.2f\n", key.c_str(), num1, len1, time1, ave1, time2, ave2, time1 / time2);
 }
 
 int main(int argc, char *argv[])
@@ -82,6 +95,10 @@ int main(int argc, char *argv[])
 		text.resize(cur + fileSize);
 		f.read(&text[cur], fileSize);
 	}
+	/* remove NUL */
+	for (size_t i = 0; i < text.size(); i++) {
+		if (text[i] == 0) text[i] = ' ';
+	}
 	text += '\0';
 	printf("text size=%d\n", (int)text.size());
 
@@ -104,7 +121,9 @@ int main(int argc, char *argv[])
 		keyTbl.push_back(key);
 	}
 
+	printf("%27s %6s %10s %8s %5s %8s %5s rate\n", "key", "count", "len", "str:Kclk", "clk/B", "qs:Kclk", "clk/B");
 	for (size_t i = 0; i < keyTbl.size(); i++) {
 		test(text, keyTbl[i]);
 	}
 }
+
