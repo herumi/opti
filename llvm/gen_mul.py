f4c5acb5430446a5fc0b449bfa168c94bf90a218
MITSUNARI Shigeo <herumi@nifty.com>
2016-05-23 10:54:30 +0900

    comparision P521

--- llvm/gen_mul.py
+++ llvm/gen_mul.py
@@ -2,7 +2,8 @@ import sys
 sys.path.insert(0, '../../mcl/src')
 import gen
 
-fo = open('mul.ll', 'w')
-gen.gen(fo, 'mul.txt', 64, [256])
+fo = open(sys.argv[1], 'w')
+unit=int(sys.argv[2])
+gen.gen(fo, 'mul.txt', unit, [unit*4])
 
 
