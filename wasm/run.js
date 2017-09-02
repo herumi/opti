3cd8c73ae0ba9a3829d1455c55797b030e57adb6
MITSUNARI Shigeo <herumi@nifty.com>
2017-09-02 18:00:19 +0900

    add test pairing

--- wasm/run.js
+++ wasm/run.js
@@ -46,7 +46,7 @@ function define_extra_functions(mod) {
 	gen_getStr = function(func) {
 		return function(x, ioMode) {
 			if (ioMode == null) { ioMode = 0 }
-			var maxBufSize = 512
+			var maxBufSize = 2048
 			var stack = mod.Runtime.stackSave()
 			var pos = mod.Runtime.stackAlloc(maxBufSize)
 			var n = func(pos, maxBufSize, x, ioMode)
@@ -82,7 +82,7 @@ function define_extra_functions(mod) {
 	}
 	gen_serialize = function(func) {
 		return function(x) {
-			var maxBufSize = 512
+			var maxBufSize = 2048
 			var stack = mod.Runtime.stackSave()
 			var pos = mod.Runtime.stackAlloc(maxBufSize)
 			var n = func(pos, maxBufSize, x)
@@ -244,4 +244,54 @@ function TestG2() {
 }
 
 function TestPairing() {
+	var a = mclBnFr_create()
+	var b = mclBnFr_create()
+	var ab = mclBnFr_create()
+	var P = mclBnG1_create()
+	var aP = mclBnG1_create()
+	var Q = mclBnG2_create()
+	var bQ = mclBnG2_create()
+	var e1 = mclBnGT_create()
+	var e2 = mclBnGT_create()
+
+	mclBnFr_setStr(a, getValue('a'))
+	mclBnFr_setStr(b, getValue('b'))
+	mclBnFr_mul(ab, a, b)
+	setText('ab', mclBnFr_getStr(ab))
+
+	mclBnG1_hashAndMapTo(P, getValue('hash_P'))
+	setText('P', mclBnG1_getStr(P))
+	mclBnG2_hashAndMapTo(Q, getValue('hash_Q'))
+	setText('Q', mclBnG2_getStr(Q))
+	mclBnG1_mul(aP, P, a)
+	setText('aP', mclBnG1_getStr(aP))
+	mclBnG2_mul(bQ, Q, b)
+	setText('bQ', mclBnG2_getStr(bQ))
+
+	mclBn_pairing(e1, P, Q);
+	setText('ePQ', mclBnGT_getStr(e1))
+	mclBn_pairing(e2, aP, bQ);
+	setText('eaPbQ', mclBnGT_getStr(e2))
+	mclBnGT_pow(e1, e1, ab)
+	setText('ePQab', mclBnGT_getStr(e1))
+	setText('verify_pairing', !!mclBnGT_isEqual(e1, e2))
+
+	const count = 50
+	var start = Date.now()
+	for (var i = 0; i < count; i++) {
+		mclBn_pairing(e1, P, Q);
+	}
+	var end = Date.now()
+	var t = (end - start) / count
+	setText('time_pairing', t)
+
+	mclBnGT_destroy(e2)
+	mclBnGT_destroy(e1)
+	mclBnG2_destroy(bQ)
+	mclBnG2_destroy(Q)
+	mclBnG1_destroy(aP)
+	mclBnG1_destroy(P)
+	mclBnFr_destroy(ab)
+	mclBnFr_destroy(b)
+	mclBnFr_destroy(a)
 }
