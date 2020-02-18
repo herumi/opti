7910c0af19ea25d8aa2e530867d2213ed05dc0d7
MITSUNARI Shigeo <herumi@nifty.com>
2020-02-18 14:36:07 +0900

    tes of vmovups with k

--- avx-512/vmovups-mask.cpp
+++ avx-512/vmovups-mask.cpp
@@ -2,6 +2,7 @@
 #include <xbyak/xbyak_util.h>
 #include <stdint.h>
 
+// return memory where base[16] can't be written if !canWrite
 uint8_t *getBoundary(bool canWrite)
 {
 	const int size = 4096;
@@ -12,14 +13,7 @@ uint8_t *getBoundary(bool canWrite)
 		base[i] = uint8_t(i);
 	}
 	if (!canWrite) {
-#ifdef _WIN32
-		DWORD old;
-		bool isOK = VirtualProtect(top + size, size, PAGE_READONLY, &old) != 0;
-#else
-		bool isOK = mprotect(top + size, size, PROT_READ) == 0;
-#endif
-		if (!isOK) {
-			perror("protect");
+		if (!Xbyak::CodeArray::protect(top + size, size, Xbyak::CodeArray::PROTECT_RE)) {
 			fprintf(stderr, "can't change access mode\n");
 			exit(1);
 		}
@@ -40,21 +34,55 @@ struct Code : Xbyak::CodeGenerator {
 			write only 15-byte size
 		*/
 	}
+	void genTestUps()
+	{
+		Xbyak::util::StackFrame sf(this, 1);
+		xorps(xm0, xm0);
+		mov(eax, 7);
+		kmovd(k1, eax);
+		vmovups(ptr[sf.p[0]]|k1, xm0);
+	}
 };
 
-void dump(const uint8_t *p, size_t n = 16)
+void dump(const void *buf, size_t n = 16)
 {
+	const uint8_t *p = (const uint8_t*)buf;
 	for (size_t i = 0; i < n; i++) {
 		printf("%02x ", p[i]);
 	}
 	printf("\n");
 }
 
+typedef void (*Func)(void*);
+
+void dump(const float *x, size_t n)
+{
+	for (size_t i = 0; i < n; i++) {
+		printf("%e ", x[i]);
+	}
+	printf("\n");
+}
+
+void testUps(Func f, uint8_t *p)
+{
+	float *x = (float *)p;
+	x[0] = 1;
+	x[1] = 2;
+	x[2] = 3;
+	puts("x");
+	dump(x, 4);
+	f(x);
+	puts("x after f");
+	dump(x, 4);
+}
+
 int main()
 	try
 {
 	Code code;
-	auto f = code.getCode<void (*)(uint8_t*)>();
+	auto f = code.getCode<Func>();
+	auto g = code.getCurr<Func>();
+	code.genTestUps();
 	uint8_t buf[256] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16 };
 	dump(buf);
 	f(buf);
@@ -64,6 +92,7 @@ int main()
 	dump(p, 20);
 	f(p + 1);
 	dump(p, 20);
+	testUps(g, p);
 } catch (std::exception& e) {
 	printf("ERR %s\n", e.what());
 	return 1;
