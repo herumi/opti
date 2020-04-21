cbcbf206224e260d8218ee6218ce65d9c3015ab3
MITSUNARI Shigeo <herumi@nifty.com>
2020-04-21 12:16:17 +0900

    test of memory

--- wasm/clang/add.cpp
+++ wasm/clang/add.cpp
@@ -7,5 +7,18 @@ API int add(int x, int y)
 	return x + y;
 }
 
+API const char *get()
+{
+	static const char *s = "abcdefg";
+	return s;
+}
+
+API const char *get2()
+{
+	static const char *s = "XYZW";
+	return s;
+}
+
+
 }
 
--- wasm/clang/test.js
+++ wasm/clang/test.js
@@ -3,7 +3,7 @@ const mod = {}
 const fs = require('fs')
 const buf = fs.readFileSync('./addsub.wasm')
 // 1 = 64KiB
-const memory = new WebAssembly.Memory({initial:1, maximum:100})
+const memory = new WebAssembly.Memory({initial:1, maximum:1})
 console.log(`memory=${memory}`)
 const imports = {
   env : {
@@ -17,10 +17,16 @@ WebAssembly.instantiate(buf, imports).then(
     const u8 = new Uint8Array(mem)
     console.log(`add=${exports.add(12, 34)}`)
     console.log(`sub=${exports.sub(12, 34)}`)
+    const p = exports.get()
+    const p2 = exports.get2()
+    console.log(`p=${p} p2=${p2}`)
     console.log(u8)
     exports.setMem(u8, 10)
     for (let i = 0; i < 10; i++) {
       console.log(`mem[${i}]=${u8[i]}`)
+    }
+    for (let i = 0; i < 10; i++) {
+      console.log(`mem[p+${i}]=${u8[p+i]}`)
     }
     console.log(`mem length=${mem.byteLength}`)
   }
